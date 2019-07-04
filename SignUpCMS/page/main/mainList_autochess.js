1d8bead1729693670bf5ad9e897c8cf0f7c5e079
David <gwwlove77@gmail.com>
2019-07-04 17:14:34 +0800

    no message

--- SignUpCMS/page/main/mainList_autochess.js
+++ SignUpCMS/page/main/mainList_autochess.js
@@ -16,11 +16,14 @@ layui.use(['form', 'layer', 'laydate', 'table', 'laytpl', 'excel','jquery'], fun
                console.log(res);
                var html = '';
                console.log(res.length);
+               html += '[';
                for (var i = 0; i < res.length; i++) {
-                   html += '{ field: '+res[i].field+', title: '+res[i].title+', align: center}';
+                   html += '{ field: "'+res[i].field+'", title: "'+res[i].title+'", align: "center"},';
                }
+               html += ']';
             dataNS = html ;
             console.log(dataNS);
+            console.log(typeof dataNS);
             },error() {
                 layer.alert('获取数据失败');
             }
