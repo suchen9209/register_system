4ca0ffc8b7c06727b58b9ef68f65ebbc4f91ea7b
David <gwwlove77@gmail.com>
2019-07-05 17:40:26 +0800

    no message

--- SignUpCMS/page/main/all.js
+++ SignUpCMS/page/main/all.js
@@ -7,14 +7,14 @@ layui.use(['form', 'layer', 'laydate', 'table', 'laytpl', 'excel', 'jquery'], fu
         laytpl = layui.laytpl,
         table = layui.table;
         excel = layui.excel;
+        state = '';
         //获取url中的参数
     function getUrlParam(name) {
         var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)"); //构造一个含有目标参数的正则表达式对象
         var r = window.location.search.substr(1).match(reg);  //匹配目标参数
         if (r != null) return unescape(r[2]); return null; //返回参数值
     }
-    var xx = getUrlParam('state');
-    alert(xx);
+    sate = getUrlParam('state');
     $.ajax({
         url: "http://apply.imbatv.cn/tool/init/list_header_json",
         type: "GET",
@@ -31,7 +31,7 @@ layui.use(['form', 'layer', 'laydate', 'table', 'laytpl', 'excel', 'jquery'], fu
             }
             var tableIns = table.render({
                 elem: '#newsList',
-                url: 'http://apply.imbatv.cn//tool/applicant?tid=3&state=-1',
+                url: 'http://apply.imbatv.cn//tool/applicant?tid=3&state='+state,
                 limit: 15,
                 limits: [15, 30, 45, 60],
                 page: true,
