b8d4a9821162af9709b61ee7d59a99a9f2e06a5d
David <gwwlove77@gmail.com>
2019-07-05 11:34:02 +0800

    no message

--- SignUpCMS/page/main/mainList_autochess.js
+++ SignUpCMS/page/main/mainList_autochess.js
@@ -25,7 +25,7 @@ layui.use(['form', 'layer', 'laydate', 'table', 'laytpl', 'excel','jquery'], fun
                html += "]'";
             dataNS = html ;
             var jsonString = '[{"name":"天鸽"},{"name":"梅花"},{"name":"台风"}]';
-            var jsonString = '[{ "field": "name", "title": "姓名"},{ "field": "nickname", "title": "昵称"},{ "field": "phone", "title": "手机号码"},{ "field": "qq", "title": "QQ"},{ "field": "extra_filed1", "title": "段位截图"}]';
+            var jsonString = [{ "field": "name", "title": "姓名"},{ "field": "nickname", "title": "昵称"},{ "field": "phone", "title": "手机号码"},{ "field": "qq", "title": "QQ"},{ "field": "extra_filed1", "title": "段位截图"}];
             console.log(dataNS);
             console.log(typeof jsonString);
             console.log(typeof dataNS);
