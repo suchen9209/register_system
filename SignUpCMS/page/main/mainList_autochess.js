a30b30d3a4e7eee9206d6dc044e07ad830320821
David <gwwlove77@gmail.com>
2019-07-05 11:48:49 +0800

    no message

--- SignUpCMS/page/main/mainList_autochess.js
+++ SignUpCMS/page/main/mainList_autochess.js
@@ -18,9 +18,9 @@ layui.use(['form', 'layer', 'laydate', 'table', 'laytpl', 'excel','jquery'], fun
                html += "[";
                for (var i = 0; i < res.length; i++) {
                 if (i == (res.length-1)) {
-                    html += '{ "field": "'+res[i].field+'", "title": "'+res[i].title+'"}'; 
+                    html += '{ "field": "'+res[i].field+'", "title": "'+res[i].title+'" ,"width": 50}'; 
                 }else{
-                   html += '{ "field": "'+res[i].field+'", "title": "'+res[i].title+'"},';
+                   html += '{ "field": "'+res[i].field+'", "title": "'+res[i].title+'","width": 50},';
                 }
                }
                 html += "]";
