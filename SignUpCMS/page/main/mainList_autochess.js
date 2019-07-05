13b85d3208f3a9caef121bc89a6492d917c7d5fa
David <gwwlove77@gmail.com>
2019-07-05 16:24:33 +0800

    no message

--- SignUpCMS/page/main/mainList_autochess.js
+++ SignUpCMS/page/main/mainList_autochess.js
@@ -19,7 +19,7 @@ layui.use(['form', 'layer', 'laydate', 'table', 'laytpl', 'excel','jquery'], fun
                     //  templet: function(d) {
                     //     return '<a href="' + d.extra_filed1 + '" target="_blank"><img src="' + d.extra_filed1 + '" height="26" /></a>';
                     // }
-                    var json = {"templet":"<div><img src='{{ d.extra_filed1}}'></div>"};
+                    var json = {"templet":"<div><a href='d.extra_filed1' target='_blank'><img src='{{ d.extra_filed1}}'></a></div>"};
                     console.log(json);
                     res.push(json);
                     }
