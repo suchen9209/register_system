9763ab46b787362f95f9684bc34606f030602459
David <gwwlove77@gmail.com>
2019-07-08 13:38:33 +0800

    no message

--- SignUpCMS/page/main/all.js
+++ SignUpCMS/page/main/all.js
@@ -26,7 +26,9 @@ layui.use(['form', 'layer', 'laydate', 'table', 'laytpl', 'excel', 'jquery'], fu
                     var json = { "title": res[i].title, "align": "center", "templet": "<div><a href='{{ d.extra_filed1}}' target='_blank'><img height='26' src='{{ d.extra_filed1}}'></a></div>" };
                     res[i] = json;
                 }else{
+                    var json1 = { "title": "fuck",};
                     res[i].align ='center';
+                    res.push =json1;
                 }
             }
             console.log(res);
