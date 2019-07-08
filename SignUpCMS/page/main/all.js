17a89f01e0b5d4c84d8614a1536b637a941a2981
David <gwwlove77@gmail.com>
2019-07-08 15:55:59 +0800

    no message

--- SignUpCMS/page/main/all.js
+++ SignUpCMS/page/main/all.js
@@ -47,11 +47,9 @@ layui.use(['form', 'layer', 'laydate', 'table', 'laytpl', 'excel', 'jquery'], fu
             var localTest = layui.data('weight');
             var json1 = { "title": '操作', "templet": "#newsListBar", "fixed": "right","align": "center"};
             console.log(localTest.weight);
-            if (localTest == 50 ) {
-                console.log(1);
+            console.log(typeof localTest.weight);
+            if (localTest.weight >= 50 ) {
                res.push(json1);  
-            }else{
-                console.log(2);
             }
             var tableIns = table.render({
                 elem: '#newsList',
