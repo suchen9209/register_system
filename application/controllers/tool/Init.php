b532063c6bc1b466bed798ad9878254d286f0618
such <suchen9209@126.com>
2019-07-09 17:04:01 +0800

    Update Init.php

--- application/controllers/tool/Init.php
+++ application/controllers/tool/Init.php
@@ -46,18 +46,19 @@ class Init extends Api_Controller {
         $menu['children'] = array();
 
         $state_show_arr = explode(',', $this->tournament_info->show_state);
-        if(in_array('-1', $state_show_arr)){
-            $menu['children'][]=array('title'=>'全部报名列表','icon'=>"&#xe61c;",'href'=>'page/main/all.html?state=-1','spread'=>true);   
-        }
-        if(in_array('5', $state_show_arr)){
-            $menu['children'][]=array('title'=>'备选列表','icon'=>"&#xe61c;",'href'=>'page/main/all.html?state=5','spread'=>true);   
-        }
-        if(in_array('10', $state_show_arr)){
-            $menu['children'][]=array('title'=>'通过列表','icon'=>"&#xe61c;",'href'=>'page/main/all.html?state=10','spread'=>true);   
-        }
         if(in_array('0', $state_show_arr)){
             $menu['children'][]=array('title'=>'未审核列表','icon'=>"&#xe61c;",'href'=>'page/main/all.html?state=0','spread'=>true);   
+        } 
+        if(in_array('10', $state_show_arr)){
+            $menu['children'][]=array('title'=>'通过列表','icon'=>"&#xe61c;",'href'=>'page/main/all.html?state=10','spread'=>true);   
+        }      
+        if(in_array('5', $state_show_arr)){
+            $menu['children'][]=array('title'=>'备选列表','icon'=>"&#xe61c;",'href'=>'page/main/all.html?state=5','spread'=>true);   
+        }        
+         if(in_array('-1', $state_show_arr)){
+            $menu['children'][]=array('title'=>'全部报名列表','icon'=>"&#xe61c;",'href'=>'page/main/all.html?state=-1','spread'=>true);   
         }
+        
 
         $return_arr['contentManagement'][]=$menu;
         
