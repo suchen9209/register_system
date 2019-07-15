d6ed78470929ad0b8f4fa783dec124b52c0938b5
such <suchen9209@126.com>
2019-07-15 17:17:42 +0800

    Update Tournament.php

--- application/controllers/tool/Tournament.php
+++ application/controllers/tool/Tournament.php
@@ -54,5 +54,15 @@ class Tournament extends Api_Controller {
         }
     }
 
+    public function setting($id=0){
+        if($id>0){
+            $return_data['tournament'] = $this->tournament->get_info($id);
+            $return_data['tournament_item'] = $this->tournament_item->get_info($id);
+            $this->response($this->getResponseData(parent::HTTP_OK, '赛事详细配置信息',$return_data), parent::HTTP_OK);
+        }else{
+            $this->response($this->getResponseData(parent::HTTP_BAD_REQUEST, '不能传空值'), parent::HTTP_OK);
+        }
+    }
+
 
 }
